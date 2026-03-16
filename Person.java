07dd58e325ef6b5ce452b45fc755476f45c7eb60
Elijah <elijah.jegede@mygeorgian.ca>
2026-03-16 11:44:15 -0400

    Add fields and basic constructor to Person

--- Person.java
+++ Person.java
@@ -1,3 +1,13 @@
 public abstract class Person {
+    // Declare fields
+    private String name;
+    private String id;
+    private String email;
 
+    // Define fields in constructor
+    Person(String name, String id, String email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
 }
