e4431120af6adb762be96a138e89f644c714b664
Elijah <elijah.jegede@mygeorgian.ca>
2026-03-16 12:05:42 -0400

    Add getters and setters with validation to Person

--- Person.java
+++ Person.java
@@ -4,10 +4,48 @@ public abstract class Person {
     private String id;
     private String email;
 
+
     // Define fields in constructor
     Person(String name, String id, String email){
         this.name = name;
         this.id = id;
+        this.email = email;
+    }
+
+    // Getter methods
+    String getName(){
+        return name;
+    }
+
+    String getId(){
+        return id;
+    }
+
+    String getEmail(){
+        return email;
+    }
+
+    // Setter methods with validation
+    void setName(String name){
+        if (name.isEmpty()){
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+
+        this.name = name;
+    }
+
+    void setId(String id){
+        if (!id.matches("S-\\d{4}")){
+            throw new IllegalArgumentException("ID must match the the following format: 'S-1234'");
+        }
+        this.id = id;
+    }
+
+    void setEmail(String email){
+        // This pattern matches email name and domain name ensuring a . is used
+        if (!email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]+$")){
+            throw new IllegalArgumentException("Invalid email");
+        }
         this.email = email;
     }
 }
