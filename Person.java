3e7dcff70dc61525a9abae7affeec14ad3ca894d
Elijah <elijah.jegede@mygeorgian.ca>
2026-03-16 12:08:59 -0400

    Add abstract method getDetails()

--- Person.java
+++ Person.java
@@ -48,4 +48,7 @@ public abstract class Person {
         }
         this.email = email;
     }
+
+    // Abstract method will be implemented in subclasses
+    public abstract String getDetails();
 }
