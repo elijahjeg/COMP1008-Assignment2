dca2f5cbe672c837a5a4b622e42674b7d6e2050d
Elijah <elijah.jegede@mygeorgian.ca>
2026-03-17 12:11:39 -0400

    Add courses functionality to student class

--- Student.java
+++ Student.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+
 public class Student extends Person {
+    private ArrayList<String> courses;
     // Constructor
     Student(String name, String id, String email){
         super(name, id, email); // Call the Person constructor with the given arguments
-
+        courses = new ArrayList<String>(); // Initialize a new ArrayList for the courses to get added later
     }
 
     // Overrides the abstract method
@@ -16,5 +19,18 @@ public class Student extends Person {
     // Regex implementation for student IDs
     protected String getIdRegex(){
         return "S-\\d{4}";
+    }
+
+    public ArrayList<String> getCourses(){
+        return courses;
+    }
+
+    public void addCourse(String course){
+        course = course.toUpperCase(); // So it's case insensitive
+        if (!course.matches("[A-Z]{4}[0-9]{4}")){ // Ex. COMP1008
+            throw new IllegalArgumentException("Invalid course code; it should follow the format: COMP1008");
+        }
+        // Valid course code, add it to the students courses
+        courses.add(course);
     }
 }
