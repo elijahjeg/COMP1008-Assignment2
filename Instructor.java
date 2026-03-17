958ef97bde3fadc581e12dd827bb55b3494b0b5a
Elijah <elijah.jegede@mygeorgian.ca>
2026-03-17 00:20:05 -0400

    Added Instructor subclass

--- Instructor.java
+++ Instructor.java
@@ -1,3 +1,19 @@
 public class Instructor extends Person {
+    // Constructor
+    Instructor (String name, String id, String email){
+        super(name, id, email);
+    }
 
+    // Overrides the abstract method
+    public String getDetails(){
+        return String.format(
+            "Instructor %s:\nInstructor ID: %s\nEmail Address: %s",
+            getName(), getId(), getEmail()
+        );
+    }
+
+    // Regex implementation for instructor IDs
+    protected String getIdRegex(){
+        return "S-\\d{4}";
+    }
 }
