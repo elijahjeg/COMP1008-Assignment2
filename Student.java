3cb67c5c26b3a6391869f29477f5f0d65002b6da
Elijah <elijah.jegede@mygeorgian.ca>
2026-03-17 12:18:55 -0400

    Added course removal by index or code with method overloading as well as get course by index

--- Student.java
+++ Student.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 
 public class Student extends Person {
+    // Declare the courses property
     private ArrayList<String> courses;
+
     // Constructor
     Student(String name, String id, String email){
         super(name, id, email); // Call the Person constructor with the given arguments
@@ -32,5 +34,19 @@ public class Student extends Person {
         }
         // Valid course code, add it to the students courses
         courses.add(course);
+    }
+
+    // Method overloading here to either remove by course code or index
+    public void removeCourse(String course){
+        courses.remove(course);
+    }
+
+    public void removeCourse(int index){
+        courses.remove(index);
+    }
+
+    // Get course by index
+    public void getCourse(int index){
+        courses.get(index);
     }
 }
